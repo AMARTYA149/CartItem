845673fc56efdfba169ddde415fb3985b634151f
Amartya Aishwarya <amartya970914@gmail.com>
2020-07-30 17:48:24 +0530

    add props to cartItem

--- src/Cart.js
+++ src/Cart.js
@@ -3,13 +3,50 @@ import CartItem from './CartItem';
 
 
 class Cart extends React.Component{
+
+  constructor() {
+    super();
+    this.state = {
+    products : [
+      {
+        price: 99,
+        title: 'Watch',
+        qty: 10,
+        img: '',
+        id: 1
+      },
+      {
+        price: 9999,
+        title: 'Mobile Phone',
+        qty: 2,
+        img: '',
+        id: 2
+      },
+      {
+        price: 9999,
+        title: 'Laptop',
+        qty: 1,
+        img: '',
+        id: 3
+      }
+    ]
+  }
+    // this.testing();
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+
   render(){
+    const { products } = this.state;
     return (
       <div className="cart">
-        <CartItem />
-        <CartItem />
-        <CartItem />
-
+        {products.map((product) => {
+        return (
+        <CartItem 
+        product = {product} 
+        key = {product.id}
+        />
+        )         
+        })}
       </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,7 @@
 import React from 'react';
 
 class CartItem extends React.Component{
-  constructor() {
-    super();
-    this.state = {
-      price: 9999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-
-    // this.testing();
-
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-  }
-
+  
   increaseQuantity = () => {    //arrow function does automatically binding of the function with the instance
     // console.log('this.state', this.state);
     // setState form 1
@@ -64,7 +51,8 @@ class CartItem extends React.Component{
   // }
 
   render(){
-    const {price , title, qty} = this.state;
+    console.log('this.props', this.props);
+    const { price, title, qty} = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
