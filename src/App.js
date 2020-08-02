e6e71c038dded783a263f3d1d540c802037fb684
Amartya Aishwarya <amartya970914@gmail.com>
2020-08-02 16:00:57 +0530

    total pricing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import { prettyDOM } from '@testing-library/react';
 
 
 class App extends React.Component {
@@ -13,21 +14,21 @@ class App extends React.Component {
         price: 99,
         title: 'Watch',
         qty: 10,
-        img: '',
+        img: 'https://images.casiocdn.com/fit-in/368x500/casio-v2/resource/images/products/watches/hd/GGB100-1A9_hd.png',
         id: 1
       },
       {
         price: 9999,
-        title: 'Mobile Phone',
+        title: 'iPhone',
         qty: 2,
-        img: '',
+        img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-max-gold-select-2019?wid=834&hei=1000&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1566953859132',
         id: 2
       },
       {
         price: 9999,
-        title: 'Laptop',
+        title: 'Macbook',
         qty: 1,
-        img: '',
+        img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-silver-select-202005?wid=892&hei=820&&qlt=80&.v=1587459986844',
         id: 3
       }
     ]
@@ -85,6 +86,18 @@ class App extends React.Component {
     return count;
   }
 
+  getTotalPrice = () => {
+    const { products } = this.state;
+
+    let total = 0;
+
+    products.forEach((product) => {
+      total += product.price * product.qty;
+    })
+
+    return total;
+  }
+
   render(){
     const { products } = this.state;
     return (
@@ -96,6 +109,7 @@ class App extends React.Component {
         onDecreaseQuantity = {this.handleDecreaseQuantity}
         onDeleteProduct = {this.handleDeleteProduct}
         />
+        <div style={{fontSize: 20, padding: 10}}>TOTAL: {this.getTotalPrice()}</div>
       </div>
     );
   }
