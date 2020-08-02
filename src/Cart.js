162c47cca4ef2b785ccce7372d1b97220befad61
Amartya Aishwarya <amartya970914@gmail.com>
2020-08-02 15:37:17 +0530

    showing total cart items

--- src/Cart.js
+++ src/Cart.js
@@ -2,79 +2,9 @@ import React from 'react';
 import CartItem from './CartItem';
 
 
-class Cart extends React.Component{
-
-  constructor() {
-    super();
-    this.state = {
-    products : [
-      {
-        price: 99,
-        title: 'Watch',
-        qty: 10,
-        img: '',
-        id: 1
-      },
-      {
-        price: 9999,
-        title: 'Mobile Phone',
-        qty: 2,
-        img: '',
-        id: 2
-      },
-      {
-        price: 9999,
-        title: 'Laptop',
-        qty: 1,
-        img: '',
-        id: 3
-      }
-    ]
-  }
-    // this.testing();
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-  }
-
-  handleIncreaseQuantity = (product) => {
-    // console.log('Increase', product);
-    const { products } = this.state;
-    const index = products.indexOf(product);
-
-    products[index].qty += 1;
-
-    this.setState({
-      products: products
-    })
-  }
-
-  handleDecreaseQuantity = (product) => {
-    // console.log('Decrease', product);
-    const { products } = this.state;
-    const index = products.indexOf(product);
-
-    if(products[index].qty === 0){
-      return;
-    }
-
-    products[index].qty -= 1;
-
-    this.setState({
-      products: products
-    })
-  }
-
-  handleDeleteProduct = (id) => {
-    const { products } = this.state;
-
-    const items = products.filter((item) => item.id !== id); //this returns array with ids not equal to id passed
-
-    this.setState({
-      products: items
-    })
-  }
-
-  render(){
-    const { products } = this.state;
+const Cart = (props) => {
+    const { products } = props;
+    // console.log("props", props);
     return (
       <div className="cart">
         {products.map((product) => {
@@ -82,15 +12,14 @@ class Cart extends React.Component{
         <CartItem 
         product = {product} 
         key = {product.id}
-        onIncreaseQuantity = {this.handleIncreaseQuantity}
-        onDecreaseQuantity = {this.handleDecreaseQuantity}
-        onDeleteProduct = {this.handleDeleteProduct}
+        onIncreaseQuantity = {props.onIncreaseQuantity}
+        onDecreaseQuantity = {props.onDecreaseQuantity}
+        onDeleteProduct = {props.onDeleteProduct}
         />
         )         
         })}
       </div>
-    );
-  }
+    );  
 }
 
 export default Cart;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const  Navbar = () => {    
+const  Navbar = (props) => {    
     return (
       <div style={styles.nav}>
         <div style={styles.cartIconContainer}>
           <img style={styles.cartIcon} src="https://image.flaticon.com/icons/svg/777/777205.svg" alt="cart-icon"/>
-          <span style={ styles.cartCount}>3</span>
+          <span style={ styles.cartCount}>{props.count}</span>
         </div>
       </div>
     );
@@ -29,7 +29,7 @@ const styles = {
   cartCount: {
     background: 'yellow',
     borderRadius: '50%',
-    padding: '2px 8px',
+    padding: '4px 8px',
     position: 'absolute',
     right: 0,
     top: -9
